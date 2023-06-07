af2b664fb868ed7881c157c05a2bb960800baead
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-07 09:53:49 +0530

    [sanjana] Add :Calculate Length and check equal

--- LineComparison/LineComputation.cs
+++ LineComparison/LineComputation.cs
@@ -11,15 +11,29 @@ namespace LineComparison
         public void CalculateLength()
         {
             Console.WriteLine("Enter x1");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int a1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter x2");
-            int b = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter y1");
-            int c = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter y2");
-            int d = Convert.ToInt32(Console.ReadLine());
-            double result = Math.Sqrt(Math.Pow((b - a), 2) + Math.Pow((d - c), 2));
-            Console.WriteLine(" length = " + result);
+            int d1 = Convert.ToInt32(Console.ReadLine());
+            double result1 = Math.Sqrt(Math.Pow((b1 - a1), 2) + Math.Pow((d1 - c1), 2));
+            Console.WriteLine("Enter x1");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            int d2 = Convert.ToInt32(Console.ReadLine());
+            double result2 = Math.Sqrt(Math.Pow((b2 - a2), 2) + Math.Pow((d2 - c2), 2));
+            checkequal(result1, result2);
+
+        }
+        public void checkequal(double a, double b)
+        {
+            Console.WriteLine(a.Equals(b));
         }
     }
 }
